c86147bad9f228bd11aa4b15a25c6063a9650556
Myles Starr <mylesstarr@gmail.com>
2021-03-24 20:45:25 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -19,15 +19,15 @@ function whileLoop(n) {
 }
 
 
-  
+
 function doWhileLoop(num) {
   let i = 0;
-   
+
   function incrementVariable() {
     i = i + 1;
     return i;
   }
-  
+
   do{
   console.log("I run once regardless.");
 } while (incrementVariable < num );
