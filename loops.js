692afea2cfb2dab698fdfa6601cd0b9c9c08417c
Myles Starr <mylesstarr@gmail.com>
2021-03-24 20:31:41 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,21 +1,11 @@
 function forLoop(array) {
-  for (let num = 0; num < 25; num++) {
+    for (let num = 0; num < 25; num++) {
           if (num === 1) {
                   array.push("I am 1 strange loop.");
           } else {
                 array.push(`I am ${num} strange loops.`);
           }
-        }
+    }
 
-        return array;
-}
-
-
-
-funtion whileLoop(n) {
-  while (n > 0) {
-          console.log(--n)
-}
-
-return "done"
+    return array;
 }
